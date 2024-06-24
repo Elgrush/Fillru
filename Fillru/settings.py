ff75d3bf7eb5c08b7ef5082da00fa0cc7cebe41d
nikarpenko1 <nikarpenko@edu.hse.ru>
2024-06-24 21:24:34 +0300

    Navigation done

--- Fillru/settings.py
+++ Fillru/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'Fillru.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', 'main/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

c1f73043c47c255549df654fb0ce5528241ce63c
Elgrush <nikikarp@mail.ru>
2024-06-25 17:02:08 +0300

    Navigation complete

--- static/js/navigation/navigation.js
+++ static/js/navigation/navigation.js
@@ -13,3 +13,25 @@ for(let element of document.getElementsByClassName(".header-nav-links"))
         });
     }
 };
+
+//Underlines telephone
+{
+    let nav_phone = document.getElementById("nav-phone");
+    nav_phone.addEventListener("mouseover", (event) => {
+        nav_phone.style["text-decoration"] = "underline";
+    })
+    nav_phone.addEventListener("mouseout", (event) => {
+        nav_phone.removeAttribute("style");
+    })
+}
+
+//Underlines icons
+for(let element of document.getElementsByClassName("header-navbar-refcontainer-icon"))
+{
+    element.addEventListener("mouseover", (event) => {
+        element.style["box-shadow"] = "inset 0 -1px 0 0";
+    })
+    element.addEventListener("mouseout", (event) => {
+        element.removeAttribute("style");
+    })
+};

5dbca1a4567460212321a540d538acda2a65cb84
Elgrush <nikikarp@mail.ru>
2024-07-04 16:40:56 +0300

    Calculator full complete

--- static/js/main/priceCalculator.js
+++ static/js/main/priceCalculator.js
@@ -32,3 +32,41 @@ fbs_input.addEventListener("input", function(){
 //Actualy calculates output for seleсt elements
 fbo_select.addEventListener("change", function() {fbo_calc_calculation();});
 fbs_select.addEventListener("change", function() {fbs_calc_calculation();});
+
+//Display calculator
+const fbo_button = document.getElementById("nav-tabs-FBO");
+const fbs_button = document.getElementById("nav-tabs-FBS");
+const fbo_calculator = document.getElementById("fbo_calc");
+const fbs_calculator = document.getElementById("fbs_calc");
+
+function select_fbo_calculator()
+{
+    fbo_calculator.removeAttribute("style");
+    fbs_calculator.style["display"] = "none";
+};
+function select_fbs_calculator()
+{
+    fbs_calculator.removeAttribute("style");
+    fbo_calculator.style["display"] = "none";
+};
+
+fbo_button.addEventListener("click", function(){
+    fbo_button.classList.add("calc-nav-active");
+    fbs_button.classList.remove("calc-nav-active");
+    select_fbo_calculator();
+});
+fbs_button.addEventListener("click", function(){
+    fbs_button.classList.add("calc-nav-active");
+    fbo_button.classList.remove("calc-nav-active");
+    select_fbs_calculator();
+});
+
+//Gives some cognitive feedback
+[fbo_button, fbs_button].forEach(element => {
+    element.addEventListener("mouseover", function(){
+        element.style["color"] = "rgb(133, 133, 133)";
+    });
+    element.addEventListener("mouseout", function(){
+        element.removeAttribute("style");
+    });
+});

db9f0d85d46635a0a79a8b52348750886a0ae80a
Elgrush <nikikarp@mail.ru>
2024-07-10 11:54:49 +0300

    Prototype ajax handler

--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "main/main.html")
+    if request.method == "GET":
+        return render(request, "main/main.html")
+    elif request.method == "POST":
+        print(request.POST)
+        return HttpResponse("Successfully")
